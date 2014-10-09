927e2f2e341e775b05136cf9ab65374a0d3ff03e
Federico Maggi <federico.maggi@gmail.com>
2014-10-10 00:55:19 +0200

    modified date

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-clay build
 parent_sha=$(git show-ref -s refs/heads/gh-pages)
 doc_sha=$(git ls-tree -d HEAD build | awk '{print $3}')
 new_commit=$(echo "Publishing." | git commit-tree $doc_sha -p $parent_sha)
